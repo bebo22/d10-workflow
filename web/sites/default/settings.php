7fa7e512deea5e41a95fefdd02be3151a8a7e6e8
Bolivar Berrio <berriobolivar@gmail.com>
2023-11-19 17:38:57 -0500

    Setting up admin theme

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -878,19 +878,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
-$databases['default']['default'] = array (
-  'database' => 'drupal_10',
-  'username' => 'dev',
-  'password' => 'stillbroken',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'isolation_level' => 'READ COMMITTED',
-  'driver' => 'mysql',
-  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
-$settings['config_sync_directory'] = 'sites/default/files/config_16BVJNz24IFhh6AdTUZVGfJvGhJw4IvDl9VSweaVvA_ncvDgHACNk0B_5pYM1221mdNinTlVVw/sync';
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+   include $app_root . '/' . $site_path . '/settings.local.php';
+}
+$settings['config_sync_directory'] = '../config';
